c04ec9d312ceff41cdc9766ac622ef3d22752b4e
Prasad Shinde <prasadshinde150904@gmail.com>
2025-08-20 10:57:33 +0530

    Update AnagramCheck.py

--- AnagramCheck.py
+++ AnagramCheck.py
@@ -1,6 +1,7 @@
 # Q: How to check if two strings are anagrams?
 def are_anagrams(s1, s2):
     return sorted(s1) == sorted(s2)
-
+    
 print(are_anagrams("listen", "silent"))  
 print(are_anagrams("key", "seen"))  
+
