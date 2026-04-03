d6aa806544e6cc54d6780d7b180923ccf4b2fa72
Prasad Shinde <prasadshinde150904@gmail.com>
2026-04-03 21:25:27 +0530

    Update Best,Average,andWorstCaseAnalysis.py

--- Best,Average,andWorstCaseAnalysis.py
+++ Best,Average,andWorstCaseAnalysis.py
@@ -5,6 +5,6 @@ def linear_search(arr, target):
     return -1
 
 arr = [5, 1, 4, 2, 8]
- 
+  
 print(linear_search(arr, 5))  # Best case: found at index 0
 print(linear_search(arr, 10)) # Worst case: not found, returns -1
