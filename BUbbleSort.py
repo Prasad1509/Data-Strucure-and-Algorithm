e62aadd2ef7f5c7f5f4c515f1774a7cd504dfdf1
Prasad Shinde <prasadshinde150904@gmail.com>
2026-07-06 16:05:27 +0530

    Update BUbbleSort.py

--- BUbbleSort.py
+++ BUbbleSort.py
@@ -2,7 +2,7 @@ arr=list(map(int,input("Enter the Numbers:").split()))
 n=len(arr)
 
 for i in range(n):
- 
+  
     for j in range(n-1-i):
         if arr[j]>arr[j+1]:
             arr[j], arr[j+1]=arr[j+1],arr[j]
