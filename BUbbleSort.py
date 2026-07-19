565cc3327daa5eb2e505a839d6ec9b2afe35ccda
Prasad Shinde <prasadshinde150904@gmail.com>
2026-07-19 21:06:34 +0530

    Update BUbbleSort.py

--- BUbbleSort.py
+++ BUbbleSort.py
@@ -5,7 +5,7 @@ for i in range(n):
   
     for j in range(n-1-i):
         if arr[j]>arr[j+1]:
-            arr[j], arr[j+1]=arr[j+1],arr[j]
+            arr[j], arr[j+1]=arr[j+1],arr[j] 
 
 print(arr)           
 
