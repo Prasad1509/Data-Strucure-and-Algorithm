ed85627e122a9c005dec44437ba64eb8414af31c
Prasad Shinde <prasadshinde150904@gmail.com>
2026-09-22 14:58:03 +0530

    Update AnagramCheck.py

--- AnagramCheck.py
+++ AnagramCheck.py
@@ -2,7 +2,7 @@
 def are_anagrams(s1, s2):
     return sorted(s1) == sorted(s2)
     
-print(are_anagrams("listen", "silent"))  
+print(are_anagrams("listen", "silent"))                
 print(are_anagrams("key", "seen"))  
  
   
